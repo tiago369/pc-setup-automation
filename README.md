f2e88e3bb5e3e81cd85186dadc860c1a8786361f
tiago369 <tiagobarreto581@gmail.com>
2022-02-13 14:13:25 -0300

    a

--- README.md
+++ README.md
@@ -2,20 +2,25 @@
 
 This repository consist in some archives used for fast configuration of my pc for robotics and personal use.
 
+## How do a dualboot in your pc
+
+![Diolinux](https://www.youtube.com/watch?v=6D6L9Wml1oY)
+
 ## OS Compatible
 Ubuntu 20.04
 
 ## Programs instaled
-1. Terminator
+1. [Terminator](https://marquesfernandes.com/desenvolvimento/otimize-seu-terminal-no-liux-com-terminator/)
 2. Git
 3. Vscode
 4. Brave Browser
 5. Element
-6. ROS Noetic
-7. Docker
-8. Opencv
-9. Latex
-10. Telegram
-11. Whatsie (Whatsapp Desktop)
-12. Tweaks
-13. Todoist
+6. [ROS Noetic](http://wiki.ros.org/ROS/Tutorials)
+7. [ROS2 Foxy](https://docs.ros.org/en/foxy/index.html)
+8. [Docker](https://docs.docker.com/)
+9. [Use docker without sudo(need to be uncommented)](https://docs.docker.com/engine/install/linux-postinstall/)
+10. [Opencv](https://www.youtube.com/watch?v=oXlwWbU8l2o)
+11. Latex
+12. Telegram
+13. Whatsie (Whatsapp Desktop)
+14. Tweaks
--- setup-pc.bash
+++ setup-pc.bash
@@ -5,13 +5,13 @@
 
 #Made for Ubuntu 20.04
 
-#Install terminator
+# Install terminator
 sudo apt install terminator
 
-#Install git
+# Install git
 sudo apt-get install git
 
-#Install vscode
+# Install vscode
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -20,35 +20,50 @@ sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
 
-#Install Brave
+# Install Brave
 sudo apt install apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
-#Install element
+# Install element
 sudo apt install -y wget apt-transport-https
 sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 sudo apt update
 sudo apt install element-desktop
 
-#Install ROS Noetic
+# Install ROS Noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full
-source /opt/ros/noetic/setup.bash
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+# source /opt/ros/noetic/setup.bash
+# echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+# source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo apt install python3-rosdep
 sudo rosdep init
 rosdep update
 
-#Install docker
+# Installing ROS 2 via Debian Packages
+locale  # check for UTF-8
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale  # verify settings
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt install ros-foxy-desktop
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install \
@@ -63,7 +78,13 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-#Install latex
+# Use docker withou sudo
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
+# docker run hello-world
+
+# Install latex
 sudo add-apt-repository ppa:jonathonf/texlive
 sudo apt update   
 sudo apt install texlive-full
@@ -92,6 +113,3 @@ sudo apt install gnome-tweak-tool
 cd %% mkdir ~/.themes
 cd && mkdir ./icons
 sudo apt install dconf-editor
-
-# Install todoist
-sudo snap install todoist
