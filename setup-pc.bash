ff698d1461130a6a64ba6e1cf5fcbad667f6db49
Tiago Barretto <tiagobarreto581@gmail.com>
2022-12-24 08:53:22 -0300

    Update structure

--- setup-pc.bash
+++ setup-pc.bash
@@ -5,111 +5,145 @@
 
 #Made for Ubuntu 20.04
 
-# Install terminator
-sudo apt install terminator
+# Run as superuser
+sudo -n true
+
+# Aks for programs
+USER=tiago
+terminator=false
+git=true
+vscode=false
+brave=true
+element=false
+noetic=false
+foxy=true
+docker=true
+latex=false
+cv2=true
 
 # Install git
-sudo apt-get install git
+if $git ; then
+  sudo apt-get install git
+fi
+
+# Install terminator
+if $terminator ; then
+  sudo apt install terminator
+fi
+
 
 # Install vscode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+if $vscode ; then
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt install apt-transport-https
+  sudo apt update
+  sudo apt install code
+fi
 
 # Install Brave
-sudo apt install apt-transport-https curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+if $brave ; then
+  sudo apt install apt-transport-https curl
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install brave-browser
+fi
 
 # Install element
-sudo apt install -y wget apt-transport-https
-sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
-sudo apt update
-sudo apt install element-desktop
+if $element ; then
+  sudo apt install -y wget apt-transport-https
+  sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
+  sudo apt update
+  sudo apt install element-desktop
+fi
 
 # Install ROS Noetic
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update
-sudo apt install ros-noetic-desktop-full
-# source /opt/ros/noetic/setup.bash
-# echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-# source ~/.bashrc
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
-sudo apt install python3-rosdep
-sudo rosdep init
-rosdep update
+if $noetic ; then
+  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+  sudo apt install curl
+  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+  sudo apt update
+  sudo apt install ros-noetic-desktop-full
+  # source /opt/ros/noetic/setup.bash
+  # echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+  # source ~/.bashrc
+  sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+  sudo apt install python3-rosdep
+  sudo rosdep init
+  rosdep update
+fi
 
-# Installing ROS 2 via Debian Packages
-locale  # check for UTF-8
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-locale  # verify settings
-sudo apt update && sudo apt install curl gnupg2 lsb-release
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-sudo apt update
-sudo apt install ros-foxy-desktop
-echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+# Installing ROS 2 Foxy
+if $foxy ; then
+  locale  # check for UTF-8
+  sudo apt update && sudo apt install locales
+  sudo locale-gen en_US en_US.UTF-8
+  sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+  export LANG=en_US.UTF-8
+  locale  # verify settings
+
+  sudo apt install software-properties-common
+  sudo add-apt-repository universe
+
+  sudo apt update && sudo apt install curl
+  sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+  sudo apt update
+  sudo apt upgrade
+  sudo apt install ros-foxy-desktop python3-argcomplete
+  sudo apt install ros-dev-tools
+fi
 
 # Install docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+if $docker ; then
+  sudo apt-get update
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# Use docker withou sudo
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# newgrp docker
-# docker run hello-world
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+fi
 
 # Install latex
-sudo add-apt-repository ppa:jonathonf/texlive
-sudo apt update   
-sudo apt install texlive-full
-tlmgr install abntex2        
-tlmgr update abntex2
-tlmgr init-usertree
+if $latex ; then
+  sudo add-apt-repository ppa:jonathonf/texlive
+  sudo apt update   
+  sudo apt install texlive-full
+  tlmgr install abntex2        
+  tlmgr update abntex2
+  tlmgr init-usertree
+fi
 
-# Install Opencv
+# # Install Opencv
+if $cv2 ; then
 sudo apt update
 sudo apt install libopencv-dev python3-opencv
 python3 -c "import cv2; print(cv2.__version__)"
-
-# Install telegram
-wget "https://telegram.org/dl/desktop/linux" -O telegram.tar.xz
-sudo tar -xvf telegram.tar.xz -C /opt
-sudo mv /opt/Telegram /opt/telegram-desktop
-sudo ln -sf /opt/telegram-desktop/Telegram /usr/bin/telegram
-echo -e '\n[Desktop Entry]\nName=Telegram\nExec=/opt/telegram-desktop/Telegram\nIcon=/opt/telegram-desktop/Telegram\nType=Application\nCategories=Network;InstantMessaging;' | sudo tee /usr/share/applications/telegram.desktop
-
-# Install Whatsapp 
-sudo snap install whatsie
+fi
 
 # Install tweaks
+if $tweaks ; then
 sudo apt install gnome-tweaks
 sudo apt install gnome-tweak-tool
 cd %% mkdir ~/.themes
 cd && mkdir ./icons
 sudo apt install dconf-editor
+fi
